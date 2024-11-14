bbaae76932d92400cced5d44f4049f0e95a7a9e1
Mohd Azam <77612776+mdazzu1261@users.noreply.github.com>
2024-11-14 15:43:05 +0530

    d

--- Assets/Scripts/Firebase/FirebaseManager.cs
+++ Assets/Scripts/Firebase/FirebaseManager.cs
@@ -16,6 +16,9 @@ public class FirebaseManager : MonoBehaviour
     private float levelStartTime;
     private float levelEndTime;
 
+
+
+
     enum LevelTrack
     {
         Start_,
@@ -53,30 +56,12 @@ public class FirebaseManager : MonoBehaviour
                // RemoteConfigManager.instance.InitializeRemoteConfig();
                 Debug.Log("Firebase initialized successfully");
                 TrackDailyEngagement();
-                // Log a start event to test
-                // LogStartEvent(PlayerPrefs.GetInt("Level", 1).ToString());
             }
             else
             {
                 Debug.LogError(String.Format("Could not resolve all Firebase dependencies: {0}", dependencyStatus));
             }
         });
-
-        //FirebaseStartTrackLevel();
-    }
-
-    public void FirebaseStartTrackLevel()
-    {
-       /* if (PlayerPrefsManager.Get_LevelNumber() == PlayerPrefs.GetInt("WINLEVELKEY", 1))
-        {
-            LogStartEvent(PlayerPrefsManager.Get_LevelNumber());
-            Debug.Log("Trackng");
-
-        }
-        else
-        {
-            Debug.Log("Already Played Start Tracking");
-        }*/
     }
 
     #endregion
@@ -119,73 +104,6 @@ public class FirebaseManager : MonoBehaviour
         {
             Debug.LogWarning("Firebase is not initialized. Event not logged.");
         }
-    }
-
-    #endregion
-
-    #region FindTheObject Game Event
-
-    public void LogStartEvent_FindTheObject(string levelName)
-    {
-        if (app != null)
-        {
-            FirebaseAnalytics.LogEvent(LevelTrack.Start_.ToString() + "FindTheObjct_" + (levelName));
-            Debug.Log("Logged start event for FindTheObject level: " + levelName);
-        }
-        else
-        {
-            Debug.LogWarning("Firebase is not initialized. Event not logged.");
-        }
-    }
-
-    public void LogWinEvent_FindTheObject(string levelName)
-    {
-        if (app != null)
-        {
-            FirebaseAnalytics.LogEvent(LevelTrack.Win_.ToString() + "FindTheObjct_" + levelName);
-            Debug.Log("Logged win event for FindTheObject level: " + levelName);
-        }
-        else
-        {
-            Debug.LogWarning("Firebase is not initialized. Event not logged.");
-        }
-    }
-
-    public void LogLoseEvent_FindTheObject(string levelName)
-    {
-        if (app != null)
-        {
-            FirebaseAnalytics.LogEvent(LevelTrack.Lose_.ToString() + "FindTheObjct_" + levelName);
-            Debug.Log("Logged lose event for FindTheObject level: " + levelName);
-        }
-        else
-        {
-            Debug.LogWarning("Firebase is not initialized. Event not logged.");
-        }
-    }
-
-    #endregion
-
-    #region Hint Used
-    public void FirbaseTrackHintUsed(int levelName)
-    {
-        hintCount++;
-        LogHintCountEvent(levelName);
-    }
-
-    private void LogHintCountEvent(int levelName)
-    {
-        FirebaseAnalytics.LogEvent("hint_used",
-             new Parameter("Level_", levelName.ToString()),
-             new Parameter("hint_count", hintCount)
-         );
-
-        Debug.Log("Hint used event logged: Level - " + levelName + ", Hint Count - " + hintCount);
-    }
-
-    public void ResetHintCount()
-    {
-        hintCount = 0;
     }
 
     #endregion
@@ -251,49 +169,6 @@ public class FirebaseManager : MonoBehaviour
            new Parameter("Level_", levelName),
            new Parameter("time_taken", timeTaken)
        );
-    }
-
-    #endregion
-
-    #region Inactive_Timer
-
-    public void FirebaseTrackInactiveTime(string levelName, float inactivityTime)
-    {
-        FirebaseAnalytics.LogEvent("level_inactivity_time",
-         new Parameter("Level_", levelName),
-         new Parameter("inactivity_time", inactivityTime)
-         );
-        Debug.Log($"Level {levelName} , {inactivityTime} seconds of inactivity.");
-    }
-
-    #endregion
-
-    #region Track_Tutorial_Complete
-
-    public void FirebaseTrackTutorialCompletion(int currentLevel, string tutorialName)
-    {
-        FirebaseAnalytics.LogEvent("tutorial_completed",
-            new Parameter("Level_", currentLevel.ToString()),  // Adjust accordingly
-            new Parameter("tutorial_name", tutorialName),  // Adjust accordingly
-            new Parameter("completion_time", Time.time) // Optionally log time
-        );
-
-        Debug.Log($"{tutorialName} Tutorial completed for Level_{currentLevel}.");
-    }
-    #endregion
-
-    #region Track_Difference
-
-    public void FirebaseTrackDifferences(int currentLevel, int currentMistakeNum, int totalMistakes)
-    {
-        if(currentLevel > 4) return;
-
-        // Log the tutorial completion event
-        FirebaseAnalytics.LogEvent("Found_Mistake",
-            new Parameter("Level_", currentLevel.ToString()),  // Adjust accordingly
-            new Parameter("Found_Mistake_", currentMistakeNum)  // Adjust accordingly
-        );
-        Debug.Log($"{currentMistakeNum} Mistake Found From {totalMistakes} for Level_{currentLevel}.");
     }
 
     #endregion
